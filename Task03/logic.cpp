66d782524f2895edbc632ca56dc44c389e171740
work204 <titanistikman@gmail.com>
2025-04-14 17:47:50 +0300

    Add project files.

---
+++ Task03/logic.cpp
@@ -0,0 +1,24 @@
+﻿// [The Odd Count of Digits]
+// Нечётное количество цифр
+//
+// Необходимо разработать рекурсивную функцию, которая проверяет,
+// состоит ли заданное пользователем число только из нечётного 
+// количества цифр или нет. Число может быть как положительным, 
+// так и отрицательным.
+// Напомним, что у числа 1234 – количество цифр чётное (even), 
+// а у числа 12345 – количество цифр нечётное (odd). 
+
+#include "logic.h"
+int count_digits(long long number);
+
+bool is_digits_count_odd(long long number) {
+	return count_digits(number) % 2 == 1;
+}
+
+int count_digits(long long number){
+	if (number < 10 && number > -10) {
+		return 1;
+	}
+
+	return count_digits(number / 10) + 1;
+}
